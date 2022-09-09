c06965e70e8b51f211b5c159652148ca50fe5a48
nitin35byte <akhileshc181@gmail.com>
2022-09-09 16:10:11 +0530

    Update

--- housing/config/configuration.py
+++ housing/config/configuration.py
@@ -2,14 +2,25 @@ from housing.entity.config_entity import DataIngestionConfig, DataTransformation
 
 from housing.util.util import read_yaml_file
 from housing.constatnt import *
+import os,sys
+from housing.exception import HousingException
+from housing.logger import logging
 
 import os
  
 ROOT_DIR = os.getcwd()     ## to get current working directory
 
 class Configuration:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,
+        config_file_path:str=CONFIG_FILE_PATH,
+        current_time_stamp =CURRENT_TIME_STAMP
+    ) -> None:
+        self.config_info = read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config = self.get_training_pipeline_config
+        self.time_stamp = current_time_stamp
+        
+
+
 
     def get_data_ingestion_config(self) ->DataIngestionConfig:
         pass
@@ -30,4 +41,14 @@ class Configuration:
         pass
 
     def get_training_pipeline_config(self) ->TrainingPipelineConfig:
-        pass
+        try:
+            training_pipeline_config=self.config_info[TRAINING_PIPELINE_CONFIG_KEY],
+            artifact_dir=os.path.join(ROOT_DIR,training_pipeline_config[TRAINING_PIPELINE_NAME_KEY],
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY])
+
+            training_pipeline_config=TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(f"training_pipeline_config:{training_pipeline_config}")
+            return training_pipeline_config
+
+        except Exception as e:
+            raise HousingException(e,sys) from e
